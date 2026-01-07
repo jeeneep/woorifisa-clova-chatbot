75a8a541185b3dcb4735d8537c60defd016c5485
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-01-07 18:00:46 +0900

    서버: try-catch 추가

--- server.js
+++ server.js
@@ -12,47 +12,53 @@ app.get('/', (_, response) => response.sendFile('index.html'));
 
 // 챗봇
 app.post('/chat', async(request, response) => {
-
-    // userId는 같은 사용자를 구분하기 위한 고유 값
-    const {userId, text} = request.body;
-
-    const payload = {
-        "version": "v2",
-        userId,
-        userIp: request.ip || "127.0.0.1",
-        "timestamp": Date.now(),
-        "bubbles": [ 
-            {
-                "type": "text",
-                "data" : { 
-                    "description" : text
+    try{
+        // userId는 같은 사용자를 구분하기 위한 고유 값-
+        const {userId, text} = request.body;
+    
+        const payload = {
+            "version": "v2",
+            userId,
+            userIp: request.ip || "127.0.0.1",
+            "timestamp": Date.now(),
+            "bubbles": [ 
+                {
+                    "type": "text",
+                    "data" : { 
+                        "description" : text
+                    } 
                 } 
-            } 
-        ],
-        "event": "send"
-    };
+            ],
+            "event": "send"
+        };
+    
+        // 외부 API 호출
+        const raw = await chatbot(payload);
+        console.log(raw);
+    
+        const result = JSON.parse(raw);
+    
+        //챗봇 응답 문자열 추출
+        const replyText =
+            (result?.bubbles ?? [])
+                .find(b => b?.type === "text")
+                ?.data?.description ?? "";
+    
+    
+        // userId와 답변 데이터
+        const data = {
+            userId: result?.userId ?? userId,
+            replyText: replyText
+        }
+    
+        // 클라이언트에게 응답 처리
+        response.send(data);
 
-    // 외부 API 호출
-    const raw = await chatbot(payload);
-    console.log(raw);
-
-    const result = JSON.parse(raw);
-
-    //챗봇 응답 문자열 추출
-    const replyText =
-        (result?.bubbles ?? [])
-            .find(b => b?.type === "text")
-            ?.data?.description ?? "";
-
-
-    // userId와 답변 데이터
-    const data = {
-        userId: result?.userId ?? userId,
-        replyText: replyText
+    } catch (err) {
+        console.error("POST /chat 오류:", err);
+        return response.status(500).send({ message: "server error" });
     }
 
-    // 클라이언트에게 응답 처리
-    response.send(data);
 
 });
 
